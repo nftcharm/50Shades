b4d4a639cf4486c1b9871831b1f9ddf03385fddf
nftcharm <nftcharm@gmail.com>
2024-07-04 21:46:59 +0200

    Added new background and nft generation
    Added new background and nft generation

--- game.js
+++ game.js
@@ -26,6 +26,12 @@ let coinSound;
 let powerUpSpeed;
 let backgroundMusic;
 let timeLeft = 60;
+let blueCoinCount = 72;
+let redCoinCount = 18;
+let greenCoinCount = 42;
+let blueCoinIncrease = 1
+let greenCoinIncrease = 3
+let redCoinIncrease = 6
 
 const speedMultiplier = 2;
 let isBoosting = false;
@@ -33,8 +39,8 @@ let currentRoom = 1;
 let background;
 
 function preload() {
-  this.load.image("background1", "/images/ground.jpg");
-  this.load.image("background2", "/images/ground2.png");
+  this.load.image("background1", "/images/ground.png");
+  // this.load.image("background2", "/images/ground2.png");
   this.load.image("player", "/images/player.png");
   this.load.image("coin", "/images/blueCoin.png");
   this.load.image("redCoin", "/images/redCoin.png");
@@ -68,6 +74,7 @@ function create() {
   }).setOrigin(1, 0);
 
   this.timeLeft = 60;
+  
   this.timerEvent = this.time.addEvent({
     delay: 1000,
     callback: updateTimer,
@@ -84,7 +91,7 @@ function create() {
 }
 
 function update() {
-  const speed = isBoosting ? 640 : 320;
+  const speed = isBoosting ? 920 : 460;
   player.setVelocity(0);
 
   if (cursors.up.isDown) {
@@ -99,9 +106,9 @@ function update() {
     player.setVelocityX(speed);
   }
 
-  if (player.x + 50 >= window.innerWidth || player.y + 50 >= window.innerHeight) {
-    changeRoom.call(this);
-  }
+  // if (player.x + 50 >= window.innerWidth || player.y + 50 >= window.innerHeight) {
+  //   changeRoom.call(this);
+  // }
 }
 
 function updateTimer() {
@@ -111,22 +118,25 @@ function updateTimer() {
     this.timeLeft = 0;
     this.timerEvent.remove();
     alert('Game Over!');
+    player.setVisible(false);
+
+    generatePixelArt.call(this, this.score);
     this.scene.pause();
   }
 
   timerText.setText('Time: ' + this.timeLeft);
 }
 
-function changeRoom() {
-  currentRoom++;
-  if (currentRoom > 2) currentRoom = 1;
+// function changeRoom() {
+//   currentRoom++;
+//   if (currentRoom > 2) currentRoom = 1;
 
-  let newBackgroundKey = `background${currentRoom}`;
-  background.setTexture(newBackgroundKey);
-  player.setPosition(window.innerWidth / 2, window.innerHeight / 2);
-}
+//   let newBackgroundKey = `background${currentRoom}`;
+//   background.setTexture(newBackgroundKey);
+//   player.setPosition(window.innerWidth / 2, window.innerHeight / 2);
+// }
 
-function spawnCoinOfType(coinType, displaySize, lifetime, scoreIncrement, bonusTime) {
+function spawnCoinOfType(coinType, displaySize, lifetime, scoreIncrement, bonusTime, destroy = true) {
   const x = Phaser.Math.Between(0, window.innerWidth);
   const y = Phaser.Math.Between(0, window.innerHeight);
 
@@ -137,23 +147,25 @@ function spawnCoinOfType(coinType, displaySize, lifetime, scoreIncrement, bonusT
     collect.call(this, player, coin, scoreIncrement, bonusTime);
   }, null, this);
 
-  this.time.delayedCall(lifetime, () => {
-    if (coin) {
-      coin.destroy();
-    }
-  });
+  if (destroy) {
+    this.time.delayedCall(lifetime, () => {
+      if (coin) {
+        coin.destroy();
+      }
+    });
+  }
 }
 
 function spawnCoin() {
-  spawnCoinOfType.call(this, "coin", { width: 40, height: 60 }, 9000, 1, 1);
+  spawnCoinOfType.call(this, "coin", { width: 60, height: 80 }, 9000, 1, 1, blueCoinIncrease);
 }
 
 function spawnGreenCoin() {
-  spawnCoinOfType.call(this, "greenCoin", { width: 40, height: 60 }, 6000, 10, 4);
+  spawnCoinOfType.call(this, "greenCoin", { width: 60, height: 80 }, 6000, 4, greenCoinIncrease);
 }
 
 function spawnRedCoin() {
-  spawnCoinOfType.call(this, "redCoin", { width: 40, height: 60 }, 3000, 20, 8);
+  spawnCoinOfType.call(this, "redCoin", { width: 60, height: 80 }, 4500, 8, redCoinIncrease);
 }
 
 function scheduleFunction(delay, callback) {
@@ -216,54 +228,77 @@ function collect(player, coin, scoreIncrement, bonusTime) {
   this.timeLeft += bonusTime;
   timerText.setText('Time: ' + this.timeLeft);
 
-  // Respawn a new coin if the collected coin was a regular coin
+  // Zähle Münzen je nach Typ
   if (coin.texture.key === 'coin') {
+    blueCoinCount += blueCoinIncrease;
     spawnCoin.call(this);
+  } else if (coin.texture.key === 'redCoin') {
+    redCoinCount += redCoinIncrease;
+  } else if (coin.texture.key === 'greenCoin') {
+    greenCoinCount += greenCoinIncrease;
   }
 }
 
-function checkScore() {
-  if (score >= 50) {
-    this.scene.pause();
+function generatePixelArt() {
+  let totalPixels = blueCoinCount + redCoinCount + greenCoinCount;
+  let pixelSize = 20;
+  let size = Math.ceil(Math.sqrt(totalPixels));
 
-    let promptBackground = this.add.rectangle(
-      window.innerWidth / 2,
-      window.innerHeight / 2,
-      400,
-      200,
-      0x000000,
-      0.8
-    );
+  let graphics = this.add.graphics({ x: window.innerWidth / 2, y: window.innerHeight / 2 });
+  graphics.clear();
+  graphics.setDepth(2);
 
-    let promptText = this.add.text(
-      window.innerWidth / 2,
-      window.innerHeight / 2 - 50,
-      "You reached 50 points! Generate NFT?",
-      { fontSize: "20px", fill: "#fff" }
-    ).setOrigin(0.5, 0.5);
+  graphics.x -= (size * pixelSize) / 2;
+  graphics.y -= (size * pixelSize) / 2;
 
-    let yesButton = this.add.text(window.innerWidth / 2 - 50, window.innerHeight / 2 + 50, "Yes", {
-      fontSize: "20px",
-      fill: "#0f0",
-    }).setOrigin(0.5, 0.5).setInteractive({ useHandCursor: true })
-      .on("pointerdown", () => {
-        promptBackground.destroy();
-        promptText.destroy();
-        yesButton.destroy();
-        noButton.destroy();
-        alert("Generating your NFT!");
-      });
+  let backgroundGraphics = this.add.graphics({ x: 0, y: 0 });
+  backgroundGraphics.fillStyle(0x000000, 1);
+  backgroundGraphics.fillRect(0, 0, window.innerWidth, window.innerHeight);
 
-    let noButton = this.add.text(window.innerWidth / 2 + 50, window.innerHeight / 2 + 50, "No", {
-      fontSize: "20px",
-      fill: "#f00",
-    }).setOrigin(0.5, 0.5).setInteractive({ useHandCursor: true })
-      .on("pointerdown", () => {
-        promptBackground.destroy();
-        promptText.destroy();
-        yesButton.destroy();
-        noButton.destroy();
-        this.scene.resume();
-      });
+  let pixels = [];
+  for (let i = 0; i < blueCoinCount; i++) pixels.push(0x0000ff);
+  for (let i = 0; i < redCoinCount; i++) pixels.push(0xff0000);
+  for (let i = 0; i < greenCoinCount; i++) pixels.push(0x00ff00);
+
+  shuffleArray(pixels);
+
+  pixels.forEach((color, index) => {
+    let x = Math.floor(index % size);
+    let y = Math.floor(index / size);
+    graphics.fillStyle(color, 1);
+    graphics.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
+  });
+
+  let textY = graphics.y + size * pixelSize + 50;
+
+  let gameOver = this.add.text(window.innerWidth / 2, textY, 'GAME OVER', {
+    fontSize: '42px',
+    fill: '#ffffff',
+    align: 'center'
+  }).setOrigin(0.5, 0);
+
+  let followText = this.add.text(window.innerWidth / 2, textY + 40, 'Follow us on X:', {
+    fontSize: '20px',
+    fill: '#ffffff',
+    align: 'center'
+  }).setOrigin(0.5, 0);
+
+  let link1 = this.add.text(window.innerWidth / 2, textY + 70, '@0xShifters', {
+    fontSize: '20px',
+    fill: '#00bfff',
+    align: 'center'
+  }).setOrigin(0.5, 0).setInteractive();
+
+  let link2 = this.add.text(window.innerWidth / 2, textY + 100, '@TED3166', {
+    fontSize: '20px',
+    fill: '#00bfff',
+    align: 'center'
+  }).setOrigin(0.5, 0).setInteractive();
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
 }
